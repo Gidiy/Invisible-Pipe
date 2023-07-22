c5d72869f56a88da3b467b44f59d37b57f34a456
Gilad Iosef <101499028+Gidiy@users.noreply.github.com>
2023-07-22 13:47:33 +0300

    Update reading_from_file_and_display.py

--- reading_and_display_wall_snake/reading_from_file_and_display.py
+++ reading_and_display_wall_snake/reading_from_file_and_display.py
@@ -7,6 +7,7 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -14,12 +15,18 @@ location_x = 0
 location_y = 0
 location_z = 0
 time_elapsed = 0
+distance = 5
 
-def vector(x_angle, y_angle, z_angle, vector_length):
-    x = vector_length * math.sin(x_angle)
-    y = vector_length * math.sin(y_angle)
-    z = vector_length * math.sin(z_angle)
+
+
+def vector(x_angle, y_angle, z_angle, length):
+
+    x = length * math.cos(x_angle) * math.cos(y_angle) #yaw =x_angle, pitch = y_angle
+    y = length * math.sin(x_angle) * math.cos(y_angle)
+    z = length * math.sin(y_angle)
+    print(f"in radian x : {x}, y:{y}, z:{z}")
     return [x, y, z]
+
 
 def degrees_to_radians(degrees):
     return degrees * math.pi / 180
@@ -28,7 +35,9 @@ def plot_vector(vec, color):
     global location_x
     global location_y
     global location_z
-
+    print (f"{vec[0],vec[1],vec[2]}")
+    text = f"{round(location_x,2)},{round(location_y,2)} , {round(location_z,2)}"
+    ax.text(location_x,location_y,location_z,text, color= 'red')
     ax.quiver(location_x, location_y, location_z, vec[0], vec[1], vec[2], color=color)
     location_x += vec[0]
     location_y += vec[1]
@@ -39,7 +48,9 @@ def clear_plot():
 
 def animate(frame):
     global time_elapsed
-    arduino_data_file = "D:/snake_wall/reading_and_display_wall_snake/txt.txt"  # file_data_location
+    global location_x
+    global distance
+    arduino_data_file = "D:/snake_wall/reading_and_display_wall_snake/txt.txt"  # file_data_location_contain_only_angels
 
     # Check if the file has been modified
     modified = os.stat(arduino_data_file).st_mtime
@@ -54,20 +65,26 @@ def animate(frame):
 
             for line in lines:
                 values = line.strip().split(',')
-
                 try:
-                    status, pitch, roll, yaw = map(int, values)
-                    plot_vector(vector(degrees_to_radians(pitch), degrees_to_radians(roll), degrees_to_radians(yaw), math.sqrt(5)), color='y')
+                    status, pitch, roll, yaw = map(int, values) #Read line from a text File with data of angle's axis
+                    print(f"pitch angle: {pitch}, roll angle:{roll}, yaw angle: {yaw}")
+                    plot_vector(vector(degrees_to_radians(-yaw), degrees_to_radians(pitch), degrees_to_radians(roll), distance),color='y')
 
+                    #data of angle's axis -> convert degrees to radians->take 3 angles in radians(Spherical coordinate system)  and length
+                    # and return cartesian vector from previous point to the next point then drew it in the 3D graph and update the last point
                 except ValueError as e:
                     print(f"Error converting values to integers: {e}")
 
+        ax.quiver(0, 0, 0, 5, 0, 0, color="r")  # x
+        ax.quiver(0, 0, 0, 0, 5, 0, color="b")  # y
+        ax.quiver(0, 0, 0, 0, 0, 5, color="g")  # z
         ax.autoscale()
         ax.set_title('SneakIn')
 
 ani = animation.FuncAnimation(fig, animate, interval=0.01, frames=10)
 
 plt.show()
+
 
 
 
